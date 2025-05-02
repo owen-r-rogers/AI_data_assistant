58d19e038e075d5b25d600de8e43da06fbb5971a
Owen Rogers <orogers@wesleyan.edu>
2025-05-02 17:27:14 -0400

    Update create_RAG_vectorstores.py

--- RAG/create_RAG_vectorstores.py
+++ RAG/create_RAG_vectorstores.py
@@ -9,15 +9,50 @@ from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings
 
 
+# load openAI API key
+load_dotenv()
+openai_api_key = os.getenv("OPENAI_API_KEY")
+
+
 # load .pdf files
 files = [f for f in os.listdir() if f.endswith('.pdf')]
+
+
+# instantiate splitter and embedding model
+splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+embedding_model = OpenAIEmbeddings()
+
 
 # Embed the contents of each file
 for file in files:
 
+    file_name = file.split('.')[0]
+
     # read and load pdf file in
     pdf_loader = PyMuPDFLoader(os.path.join(os.getcwd(), file))
-    pdf_docs = pdf_loader.load()
+    pdf_doc = pdf_loader.load()
+
+    # split documents
+    split_docs = splitter.split_documents(pdf_doc)
+
+    # create vector store
+    vectorstore = FAISS.from_documents(pdf_doc, embedding=embedding_model)
+
+    # save vectorstore and metadata
+    vectorstore.save_local(f'./vectorstore/faiss_index/{file_name}')
+    with open(f'./vectorstore/faiss_index/{file_name}.pkl', 'wb') as f:
+        pickle.dump(vectorstore, f)
+
+    # get the FAISS index object
+    faiss_index = vectorstore.index
+
+    # save embeddings to a numpy array
+    vectors = faiss_index.reconstruct_n(0, faiss_index.ntotal)
+    np.save(f'vectorstore/rag_embeddings_{file_name}.npy', vectors)
+
+    # add a reference section from documents to the vector store
+    ids = vectorstore.add_documents(documents=pdf_doc)
+
 
 
 
