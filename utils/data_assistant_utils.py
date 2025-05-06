d193876d52e0b6cee9583c83946812ce79ebcf52
Owen Rogers <orogers@wesleyan.edu>
2025-05-06 13:22:22 -0400

    Update data_assistant_utils.py

--- utils/data_assistant_utils.py
+++ utils/data_assistant_utils.py
@@ -288,7 +288,11 @@ def process_stream(stream, save=True, save_name='BLAST_results'):
         blast_accessions['title'].append(record)
 
         rec = fetch_sequence(record, email='orogers@wesleyan.edu')
-        blast_accessions['sequence'].append(str(rec.seq))
+        seq = rec.seq
+        if seq is None:
+            blast_accessions['sequence'].append('')
+        else:
+            blast_accessions['sequence'].append(str(seq))
 
         try:
             blast_accessions['summary'].append(rec.annotations['comment'])
